5ec86638128bcef9c779d7ae92d3d8ed845887e8
chupaaaaaaan <jacobitebeerlove@yahoo.co.jp>
2019-02-24 17:07:12 +0900

    モリッと修正
    - 名前付きブロック記法の使用
    - GHC_VERSIONの動的取得→リストファイルのフォーマット変更
    - HIEで「cabal-install」パッケージインストール時に、nightly ではなく指定したGHC_VERSIONに対応するresolverを使うよう修正

--- __base/haskell/build.sh
+++ __base/haskell/build.sh
@@ -2,15 +2,11 @@
 
 cd $(dirname $0)
 
-while read line
+while read RESOLVER
 do
-    RESOLVER=$(echo ${line} | tr -s ' ' | cut -d' ' -f1)
-    GHC_VERSION=$(echo ${line} | tr -s ' ' | cut -d' ' -f2)
     TAG="stack:${RESOLVER}"
-
     if [ $(docker images | grep "${TAG}" | wc -l) -eq 0 ]; then
         docker build . -t "${TAG}" \
-               --build-arg "RESOLVER=${RESOLVER}" \
-               --build-arg "GHC_VERSION=${GHC_VERSION}"
+               --build-arg "RESOLVER=${RESOLVER}"
     fi
 done < <(cat resolver-list.txt | grep -v -e "^[[:space:]]*#" -e "^[[:space:]]*$")
